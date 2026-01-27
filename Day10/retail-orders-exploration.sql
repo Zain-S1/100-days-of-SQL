24227defcc88bc86398b0f11a8825ca2b9e3221e
Zainab <z.alkholaif@gmail.com>
2026-01-27 22:52:18 +0300

    Update retail-orders-exploration.sql

--- Day10/retail-orders-exploration.sql
+++ Day10/retail-orders-exploration.sql
@@ -1,1 +1,7 @@
+-- Day 10: Kaggle Dataset Exploration — Retail Orders
 
+-- Columns:
+-- Order Id, Order Date, Ship Mode, Segment,
+-- Country, City, State, Postal Code, Region,
+-- Category, Sub Category, Product Id,
+-- cost price, List Price, Quantity, Discount Percent
