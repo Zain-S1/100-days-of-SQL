49986bb07d938d103bc416a5cf5e6c66f5f4e386
Zainab <z.alkholaif@gmail.com>
2026-01-28 22:32:34 +0300

    Update LeetCode-1141.sql

--- Day11/LeetCode-1141.sql
+++ Day11/LeetCode-1141.sql
@@ -1,13 +1,16 @@
--- Day 1: Filtering Products by Business Rules
+-- Day 11: Daily Active Users (DAU)
 
 -- Question:
--- Which products meet both health and sustainability criteria?
+-- How many unique users were active each day during the
+-- 30-day period ending on 2019-07-27 (inclusive)?
 
 -- Solution
-SELECT product_id
-FROM Products
-WHERE low_fats = 'Y' 
-    AND recyclable = 'Y';
+SELECT 
+    activity_date AS day, 
+    COUNT(DISTINCT(user_id)) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date;
 
 -- Source:
--- LeetCode 1757 — Recyclable and Low Fat Products
+-- LeetCode 1141 — User Activity for the Past 30 Days I
