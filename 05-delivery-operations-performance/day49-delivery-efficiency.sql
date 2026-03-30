bc5eac28cc3207c147fe7572df3faf1f03c9aaff
Zainab <z.alkholaif@gmail.com>
2026-03-30 21:11:20 +0300

    Update day49-delivery-efficiency.sql

--- 05-delivery-operations-performance/day49-delivery-efficiency.sql
+++ 05-delivery-operations-performance/day49-delivery-efficiency.sql
@@ -1,1 +1,55 @@
+-- Day 47: Drivers of Customer Satisfaction
 
+-- Question:
+-- What factors impact delivery ratings?
+
+-- Solution
+--------------------------------------------------
+-- 1️⃣ Rating by Delay Status
+--------------------------------------------------
+SELECT
+    CASE
+        WHEN delivery_time_hours > expected_time_hours THEN 'Delayed'
+        ELSE 'On-Time'
+    END AS delivery_status_group,
+    AVG(delivery_rating) AS avg_rating,
+    COUNT(*) AS deliveries
+FROM deliveries
+GROUP BY delivery_status_group;
+
+--------------------------------------------------
+-- 2️⃣ Rating by Delivery Time Bucket
+--------------------------------------------------
+SELECT
+    CASE
+        WHEN delivery_time_hours < 3 THEN '< 3 hrs'
+        WHEN delivery_time_hours < 6 THEN '3-6 hrs'
+        WHEN delivery_time_hours < 10 THEN '6-10 hrs'
+        ELSE '10+ hrs'
+    END AS time_bucket,
+    AVG(delivery_rating) AS avg_rating
+FROM deliveries
+GROUP BY time_bucket
+ORDER BY avg_rating DESC;
+
+--------------------------------------------------
+-- 3️⃣ Rating by Delivery Cost Bucket
+--------------------------------------------------
+SELECT
+    CASE
+        WHEN delivery_cost < 500 THEN 'Low Cost'
+        WHEN delivery_cost < 1000 THEN 'Medium Cost'
+        ELSE 'High Cost'
+    END AS cost_bucket,
+    AVG(delivery_rating) AS avg_rating
+FROM deliveries
+GROUP BY cost_bucket
+ORDER BY avg_rating DESC;
+
+-- Inisghts:
+-- * Delays are the single biggest driver of customer dissatisfaction
+-- * Ratings decrease as cost increases
+
+--------------------------------------------------
+-- Source:
+-- Kaggle Dataset — Delivery Logistics Performance & Operations
