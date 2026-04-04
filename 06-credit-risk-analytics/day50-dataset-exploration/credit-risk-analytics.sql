d326646a652be01c121b03527069ede72c0c5f89
Zainab <z.alkholaif@gmail.com>
2026-04-04 11:40:31 +0300

    Update credit-risk-analytics.sql

--- 06-credit-risk-analytics/day50-dataset-exploration/credit-risk-analytics.sql
+++ 06-credit-risk-analytics/day50-dataset-exploration/credit-risk-analytics.sql
@@ -3,10 +3,11 @@
 
 -- Columns:
 
--- age, job, marital, education, defult
--- balance, housing, loan, contact, day,
--- month, diration, campaign, pdays, 
--- previous, pouycome, y
+-- age, job, marital_status, education_level, has_credit_defult, 
+-- balance, has_housing_loan, has_personal_loan, 
+-- contact_type, contact_day, contact_month, call_duration_seconds, 
+-- campaign_contacts, days_since_last_contact, previous_contacts, 
+-- previous_campaign_outcome, subscribed
 
 --------------------------------------------------
 -- 1. Total Clients
