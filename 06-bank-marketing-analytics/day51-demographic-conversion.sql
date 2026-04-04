3be09fbb55e1996c312294d177cb5a5fa82ef53c
Zainab <z.alkholaif@gmail.com>
2026-04-04 12:38:15 +0300

    Update day51-demographic-conversion.sql

--- 06-bank-marketing-analytics/day51-demographic-conversion.sql
+++ 06-bank-marketing-analytics/day51-demographic-conversion.sql
@@ -9,7 +9,14 @@
 --------------------------------------------------
 -- 2️⃣ Conversion by Job Type
 --------------------------------------------------
-
+SELECT 
+   job, 
+   COUNT(*) AS total_clients,
+   SUM(CASE WHEN subscribed = 'yes' THEN 1 ELSE 0 END) * 1.0
+   / COUNT(*) AS conversion_rate
+FROM credit_scoring
+GROUP BY job
+ORDER BY conversion_rate DESC;
 
 --------------------------------------------------
 -- 3️⃣ Conversion by Education Level
