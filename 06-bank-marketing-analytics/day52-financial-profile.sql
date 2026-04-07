329d5021fe13b982ff72002e6f57291b319e671d
Zainab <z.alkholaif@gmail.com>
2026-04-07 15:42:33 +0300

    Update day52-financial-profile.sql

--- 06-bank-marketing-analytics/day52-financial-profile.sql
+++ 06-bank-marketing-analytics/day52-financial-profile.sql
@@ -7,7 +7,19 @@
 --------------------------------------------------
 -- 1️⃣ Conversion by Account Balance Bucket
 --------------------------------------------------
-
+SELECT 
+   CASE
+      WHEN balance < 0 THEN 'Negative'
+      WHEN balance < 1000 THEN '0-1K'
+      WHEN balance < 5000 THEN '1K-5K'
+      ELSE '5K+'
+   END AS balance_group, 
+   COUNT(*) AS total_clients,
+   SUM(CASE WHEN subscribed = 'yes' THEN 1 ELSE 0 END) * 1.0
+   / COUNT(*) AS conversion_rate
+FROM credit_scoring
+GROUP BY balance_group
+ORDER BY conversion_rate DESC;
 
 --------------------------------------------------
 -- 2️⃣ Conversion by Credit Defult
